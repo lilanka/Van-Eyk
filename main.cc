bfd61cc4c9bbc7c0bde3f6327fb935f79d0e9d9e
lilanka <pathirage.lilanka@gmail.com>
2021-03-25 03:40:50 +0530

    first working project. forward with tensor creation

--- main.cc
+++ main.cc
@@ -4,6 +4,24 @@
 #include <xtensor/xio.hpp>
 #include <xtensor/xtensor.hpp>
 
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+// Don't fuck with names in c++, 
+
+int add(int i, int j) {return i + j;}
+
+xt::xarray<double> addd() {
+  xt::xarray<double> a = {{1., 2.}, {3., 4.}};
+  return a;
+}
+
+PYBIND11_MODULE(go, m) {
+  m.doc() = "pybind11 example pluging";
+  m.def("add", &add, "a function with add two numbers");
+}
+
 int main() {
   xt::xarray<double> a = {{1., 2.}, {3., 4.}};
   std::cout << a << std::endl;
