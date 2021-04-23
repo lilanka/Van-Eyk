minor changes. Fix backprop algorithm

test.py
@@ -8,5 +8,5 @@ b = Tensor(np.array([6, 4]), requires_grad=True)
 Q = (a**3)*3 - b**2
 
 print(Q)
-Q.backward()
+#Q.backward()
 #print(a.grad)